918bd040dbe39c0f1fbd66cfbd1bdba758716365
Saifalibhimani <94796479+Saifalibhimani@users.noreply.github.com>
2021-12-06 21:18:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,185 @@
+// // var a=prompt()
+// // var b= 18
+
+// // if(a > b) {
+// //     console.log("User Allowed");
+// // } else {
+// //   if(c) {
+// //       console.log("UserAllowed on Card")
+// //  } else{  
+// //     console.log("User Not Allowed")
+// // }
+// // }
+
+// // var age = prompt
+// // var age = true;
+// // var t= "saims"
+
+// // if (age > 18 || c == true) {
+// //     console.log("User Allowed");
+// // }
+
+// // if(c== true && t == "saims"){
+// //     console.log ("User Allowed");
+// // } 
+// // && and operator
+// // || or operator
+
+
+
+// var a = "apple";
+// var b = "orange";
+// var c = "pineapple";
+// var d = "mango";
+// var e = "shareefa";
+
+// var fruits = ["apple","mango","orange"];
+  
+// fruits[2] = "asdasda"
+
+// console.log(fruits);
+
+//  var arr =["abc", 123,true,prompt()]
+
+//  console.log(arr);
+// arr.push("abc");
+// arr.push(123);
+// arr.push(true);
+// arr.pop();
+// arr.unshift("xyz");
+// arr.shift();
+// var newArrayDeleted =arr.splice(3,4);
+// var newArray 
+
+// var arr =["a","b","c","d"];
+
+// var word ='c'
+// var input =prompt();
+
+// for(var i =0; i < arr.length; i++) {
+//     if (word == arr[i]) {
+   
+//     }
+// }
+
+// var date = new Date();
+
+// var year =
+
+// console.log(date);
+
+// var a = +"120.444"
+
+// var b = +a;
+// var b = Number(a)
+// var b = parseFloat(a)
+// var b = parseInt(a)
+
+// console.log(b);
+
+// var a = 4560;
+
+// var b = a.toString();
+
+// console.log(b);
+
+// var dateNow =new Date()
+
+// var year = dateNow.getFullYear();
+// var month = dateNow.getMonth();
+// var date = dateNow.getDate();
+// var day = dateNow.getDay();
+// var hours = dateNow.getHours();
+// var min = dateNow.getMinutes();
+// var Seconds = dateNow.getSeconds();
+// var millisec = dateNow.getMilliseconds();
+// var time = dateNow.getTime();
+
+
+
+// var months = ["","","",""]
+
+// console.log(dateNow);
+// console.log(month);
+// console.log(year);
+// console.log(date);
+// console.log(days);
+// console.log(days[day]);
+
+// var currentDate = new Date();
+// var currentDatetime = currentDate.getTime();
+// console.log(currentDatetime);
+
+// var birthdaygiven = prompt("date of birth")
+// var userbirthday =new Date(birthdaygiven)
+// var userbirthdaytime = userbirthday.getTime()
+
+// var diffMiliSec = currentDatetime - userbirthdaytime;
+// var age = diffMiliSec / (1000 * 60 * 60* 24 * 365);
+
+// console.log(age);
+
+var currentDate = new Date();
+var newupdatedate = currentDate.setFullYear(1997)
+var newupdatedate = currentDate.setMonth(5)
+var newupdatedate = currentDate.setDate(17)
+var newupdatedate = currentDate.setHours(17)
+var newupdatedate = currentDate.setMinutes(30)
+//  var newupdatedate = currentDate.setSeconds(20)
+
+
+
+console.log(currentDate)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
